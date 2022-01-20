e16ff18ba6a2bab69952cef979e07260ace79a6d
Xuan <lydia.xuan.chen@gmail.com>
2022-01-20 00:05:06 -0500

    Delete inverTracker directory

--- inverTracker/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO products(pname, supplier, price, stock) values('toilet paper', 'royale', '20', '30');
-INSERT INTO products(pname, supplier, price, stock) values('glue', 'elmer', '3', '30');
-INSERT INTO products(pname, supplier, price, stock) values('bread', 'wonder', '3', '10');
